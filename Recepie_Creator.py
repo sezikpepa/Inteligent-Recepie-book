c20661c5c9cbaf831a5d42e7053bead02a0df2e0
Sezemský Josef <sezikpepak@gmail.com>
2023-05-03 11:00:10 +0200

    create recepie creator structure

---
+++ Recepie_Creator.py
@@ -0,0 +1,14 @@
+from recepie import Recepie
+
+class Recepie_Creator:
+	def __init__(self) -> None:
+		recepies: list = [] 
+
+	def LoadFile(file_path: str):
+		pass
+
+	def ParseFile():
+		pass
+
+	def ParseRecepie():
+		pass
