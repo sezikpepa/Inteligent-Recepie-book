9c5018845d16973d0ad607e909c23f3e3ff13735
Sezemský Josef <sezikpepak@gmail.com>
2023-05-03 21:07:39 +0200

    parse ingredience to basic forms

--- IngredienceTaker.py
+++ IngredienceTaker.py
@@ -1,3 +1,90 @@
+import re
+
 class IngredienceTaker:
-	def __init__(self) -> None:
-		pass		
+	def __init__(self, ingredience: list) -> None:
+		self.ingredience = ingredience 
+
+
+	def remove_things_in_brackets(self):
+		new_list = []
+
+		for element in self.ingredience:
+			new_list.append(re.sub("\(.*?\)","",element))
+
+		self.ingredience = new_list
+
+	def remove_non_letters(self):
+		valid_letters = "qwertzuioplkjhgfdsayxcvbnmQWERTZUIOPLKJHGFDSAYXCVBNM "
+		new_list = []
+
+		for element in self.ingredience:
+			new_list.append(''.join([i for i in element if i in valid_letters]))
+
+		self.ingredience = new_list
+
+	def remove_after(self):
+		new_list = []
+
+		for element in self.ingredience:
+			toappend = element.split(",")[0]
+			toappend = toappend.split(" or ")[0]
+			toappend = toappend.split(" such as ")[0]
+			toappend = toappend.split(" from ")[0]
+			toappend = toappend.split(" but ")[0]
+			new_list.append(toappend)
+
+		self.ingredience = new_list
+
+	def remove_nonneeded_words(self):
+		new_list = []
+
+		words_without_meaning = ["of", "to", "other", "plus", "a", "an", "for", "by", "enough", "up", "about"]
+		colors = ["white", "black", "purple", "red"]
+		units = ["oz", "lb", "kg", "qt", "pound", "pounds", "g", "ounces", "aj", "one", "ounce", "bag", "bags", "inch", "single"]
+		size_words = ["small", "medium", "big", "large", "whole", "head", "inchthick", "mini", "mediumsize"]
+		things_sizes = ["bunch", "package", "packages", "tsp", "tbsp", "cup", "teaspoon", "teaspoons", "pan","tablespoon", "tablespoons","can", "jar", "jars", "cans", "cups"]
+		adjectives = ["accompaniments","heavy","wide","accompaniment","centercut", "packed", "loosely", "canned", "mixed", "warm", "skinless", "heaped", "zest", "good", "generous", "amount", "boiling", "sharp", "coarsely", "thinly", "sliced", "wavy", "pure", "regular", "ground", "rrated", "smoked", "finely", "chopped", "freshly", "fresh", "hot", "cold", "diced", "crushed", "grated", "roasted", "pickled", "roughly", "toasted", "extra"]
+
+		nonneeded_words = size_words + things_sizes + colors + adjectives + words_without_meaning + units
+
+		for element in self.ingredience:
+			new_list.append(''.join([i + " " for i in element.split(" ") if i not in nonneeded_words]))
+
+		self.ingredience = new_list
+
+	def to_lower_case(self):
+		new_list = []
+
+		for element in self.ingredience:
+			new_list.append(element.lower())
+
+		self.ingredience = new_list
+
+	def remove_white_speces_around(self):
+		new_list = []
+
+		for element in self.ingredience:
+			to_append = element.strip()
+			to_append = re.sub("  ", " ", to_append)
+			new_list.append(element.strip())
+
+		self.ingredience = new_list
+
+	def remove_duplicities(self):
+		self.ingredience = list(set(self.ingredience))
+
+	def remove_long_ingredience(self):
+		new_list = []
+
+		for element in self.ingredience:
+			if len(element) < 40:
+				new_list.append(element.strip())
+		self.ingredience = new_list
+
+
+	def __str__(self):
+		toReturn = ""
+		for element in self.ingredience:
+			toReturn += element + "\n"
+
+		return toReturn
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from Recepie_Creator import Recepie_Creator
+from IngredienceTaker import IngredienceTaker
 
 if __name__ == '__main__':
 	recepie_creator = Recepie_Creator()
 
 	recepie_creator.LoadFile("C:\\Users\\sezik\\OneDrive - Univerzita Karlova\\projekty\\Inteligent-Recepie-book\\archive\\" + "dataset_small.csv")
 
-	print(recepie_creator)
+	for i in range(13000):
+		ingredience_taker = IngredienceTaker(recepie_creator.recepies[i].ingredience)
+		ingredience_taker.remove_things_in_brackets()
+		ingredience_taker.remove_after()
+		ingredience_taker.to_lower_case()
+		ingredience_taker.remove_non_letters()
+		ingredience_taker.remove_nonneeded_words()
+		ingredience_taker.remove_white_speces_around()
+		ingredience_taker.remove_duplicities()
+		ingredience_taker.remove_white_speces_around()
+		ingredience_taker.remove_long_ingredience()
+		print(ingredience_taker)
+	
+
+	print(ingredience_taker)
